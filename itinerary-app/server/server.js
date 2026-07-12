828d1c329c9b31f6ccb74bad24624d1bb8d2182c
Rques0 <arqueso@tutamail.com>
2026-07-11 20:16:41 -0700

    Serve frontend static files from Express

--- itinerary-app/server/server.js
+++ itinerary-app/server/server.js
@@ -1,13 +1,17 @@
 import 'dotenv/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import express from 'express';
 import cors from 'cors';
 import { getRoutingProvider } from './routingProvider.js';
 import { planTrip, enrichPlan } from './aiAgent.js';
 import { listTrips, getTrip, saveTrip, deleteTrip, getAtlas } from './store.js';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..')));
 
 const routingProvider = getRoutingProvider();
 
